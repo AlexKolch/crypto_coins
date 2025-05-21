48922240cd5f1e4b082bc715357f2c2578650184
Alex Kolch <alexkolch.ios@gmail.com>
2025-05-21 12:22:34 +0300

    ListView add

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Crypto coins',
       theme: ThemeData(
-scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 31, 31, 31),
+          titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold 
+        )
+        ),
+        dividerColor: Colors.white24,
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+        textTheme: TextTheme(
+          bodyMedium: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
+          labelSmall: TextStyle(
+            color: Colors.white.withValues(alpha: 0.6),
+            fontWeight: FontWeight.w700,
+            fontSize: 14,
+          ),
+        ),
       ),
       home: const MyHomePage(title: 'Crypto coins'),
     );
@@ -31,38 +53,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: ListView.separated(
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: 10, //Кол-во ячеек
+        itemBuilder:
+            (context, index) => ListTile(
+              leading: Image.asset('assets/img/bitcoin.png', width: 25, height: 25),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              title: Text('Bitcoin', style: theme.textTheme.bodyMedium),
+              subtitle: Text('2000\$', style: theme.textTheme.labelSmall)
             ),
-          ],
-        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
